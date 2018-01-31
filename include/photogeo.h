82d0aed31afd121ba4daeafee71836885c0f8a43
Albin Bernhardsson <catscratchesp@gmail.com>
2018-01-31 15:25:15 +0100

    Project structure

---
+++ include/photogeo.h
@@ -0,0 +1,32 @@
+#ifndef PHOTOGEO_H
+#define PHOTOGEO_H
+
+// Functions need to be imported/exported when compiling a shared library (DLL) on Windows.
+#ifdef _WIN32
+    #ifdef photgeo_EXPORTS
+        #define PHOTOGEO_API __declspec(dllexport)
+    #else
+        #define PHOTOGEO_API __declspec(dllimport)
+    #endif
+#else
+    #define PHOTOGEO_API
+#endif
+
+// Export C API.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Sum two numbers.
+ * @param x First number.
+ * @param y Second number.
+ * @return The sum of the numbers.
+ */
+int ptg_add(int x, int y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/photogeo.cpp
@@ -0,0 +1,5 @@
+#include <photogeo.h>
+
+int ptg_add(int x, int y) {
+    return x + y;
+}
