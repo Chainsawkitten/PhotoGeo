2c9ce3db1525feaa138b222dbd4062ac35a2be7a
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-08 13:07:24 +0100

    Handle commandline arguments

--- tool/main.cpp
+++ tool/main.cpp
@@ -3,6 +3,25 @@
 #include <cstring>
 
 int main(int argc, const char* argv[]) {
+    // Handle commandline arguments.
+    const char* inputFilename = "";
+    const char* outputFilename = "";
+    for (int argument = 1; argument < argc; ++argument) {
+        // All arguments start with -.
+        if (argv[argument][0] == '-') {
+            // Input filename.
+            if (argv[argument][1] == 'i' && argc > argument + 1)
+                inputFilename = argv[++argument];
+
+            // Output filename.
+            if (argv[argument][1] == 'o' && argc > argument + 1)
+                outputFilename = argv[++argument];
+        }
+    }
+
+    std::cout << "Input: " << inputFilename << std::endl;
+    std::cout << "Output: " << outputFilename << std::endl;
+
     // Source image info.
     ptg_source_parameters parameters;
     memset(&parameters, 0, sizeof(ptg_source_parameters));
