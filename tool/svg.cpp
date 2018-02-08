01ecc0cb36306a3500f8c22ed8c2fc241a73e83e
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-08 14:36:37 +0100

    WriteSVG stub

---
+++ tool/svg.cpp
@@ -0,0 +1,7 @@
+#include "svg.hpp"
+
+#include <iostream>
+
+void WriteSVG(unsigned int layers, ptg_color* colors, unsigned int* vertexCount, ptg_vec2** vertices) {
+    std::cout << "WriteSVG not yet implemented" << std::endl;
+}
