dba4319f7330acabcdf18ce3209a620c10d31dcf
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-08 11:55:39 +0100

    Generate geometry stub

--- src/photogeo.cpp
+++ src/photogeo.cpp
@@ -1,5 +1,14 @@
 #include <photogeo.h>
 
-int ptg_add(int x, int y) {
-    return x + y;
+#include <iostream>
+
+void ptg_generate_collision_geometry(const ptg_source_parameters* parameters, unsigned int** out_vertex_count, ptg_vec2** out_vertices) {
+    std::cout << "ptg_generate_collision_geometry has not yet been implemented." << std::endl;
+
+    *out_vertex_count = nullptr;
+    *out_vertices = nullptr;
+}
+
+void ptg_free_results(unsigned int* vertex_count, ptg_vec2* vertices) {
+    std::cout << "ptg_free_results has not yet been implemented." << std::endl;
 }
