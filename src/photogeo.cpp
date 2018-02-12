3689e88c03bb102a8685bad655d54c9bff058cb9
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-12 15:27:17 +0100

    quantize_euclidean stub

--- src/photogeo.cpp
+++ src/photogeo.cpp
@@ -1,13 +1,14 @@
 #include <photogeo.h>
 
 #include <iostream>
+#include "quantization/euclidean.hpp"
 
 void ptg_generate_collision_geometry(const ptg_generation_parameters* parameters, unsigned int** out_vertex_count, ptg_vec2*** out_vertices) {
     std::cerr << "ptg_generate_collision_geometry has not yet been implemented." << std::endl;
 
     // Quantization.
-    ptg_color** layers = ptg_quantize(parameters->image_parameters, parameters->quantization_parameters);
-    ptg_free_quantization_results(layers);
+    bool** layers = ptg_quantize(parameters->image_parameters, parameters->quantization_parameters);
+    ptg_free_quantization_results(layers, parameters->image_parameters->color_layer_count);
 
     *out_vertex_count = nullptr;
     *out_vertices = nullptr;
@@ -17,12 +18,22 @@ void ptg_free_results(unsigned int* vertex_count, ptg_vec2** vertices) {
     std::cerr << "ptg_free_results has not yet been implemented." << std::endl;
 }
 
-ptg_color** ptg_quantize(const ptg_image_parameters* image_parameters, const ptg_quantization_parameters* quantization_parameters) {
+bool** ptg_quantize(const ptg_image_parameters* image_parameters, const ptg_quantization_parameters* quantization_parameters) {
     std::cerr << "ptg_quantize has not yet been implemented." << std::endl;
 
-    return nullptr;
+    // Allocate color layers.
+    bool** layers = new bool*[image_parameters->color_layer_count];
+    for (unsigned int layer = 0; image_parameters->color_layer_count; ++layer)
+        layers[layer] = new bool[image_parameters->width * image_parameters->height];
+
+    // Quantize image into layers.
+    quantize_euclidean(image_parameters, layers);
+
+    return layers;
 }
 
-void ptg_free_quantization_results(ptg_color** layers) {
-    std::cerr << "ptg_free_quantization_results has not yet been implemented." << std::endl;
+void ptg_free_quantization_results(bool** layers, unsigned int layer_count) {
+    for (unsigned int layer = 0; layer < layer_count; ++layer)
+        delete[] layers[layer];
+    delete[] layers;
 }
---
+++ src/quantization/euclidean.cpp
@@ -0,0 +1,7 @@
+#include "euclidean.hpp"
+
+#include <iostream>
+
+void quantize_euclidean(const ptg_image_parameters* parameters, bool** layers) {
+    std::cerr << "quantize_euclidean has not yet been implemented." << std::endl;
+}
