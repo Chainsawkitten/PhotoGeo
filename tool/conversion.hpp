afb5033f0b1d64cf1416f8ab9242a4a2cf785258
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-12 16:16:15 +0100

    Hand color commandline parameters

---
+++ tool/conversion.hpp
@@ -0,0 +1,13 @@
+#ifndef CONVERSION_HPP
+#define CONVERSION_HPP
+
+#include <photogeo.h>
+
+/**
+ * Get color from text representation.
+ * @param text The text to convert.
+ * @return The color.
+ */
+ptg_color TextToColor(const char* text);
+
+#endif
