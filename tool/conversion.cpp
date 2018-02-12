1fa1fbcbbff4f182f38beb2d4a4c81fd89a020cb
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-12 16:25:47 +0100

    Implement TextToColor

--- tool/conversion.cpp
+++ tool/conversion.cpp
@@ -1,14 +1,24 @@
 #include "conversion.hpp"
 
-#include <iostream>
-
 ptg_color TextToColor(const char* text) {
-    std::cerr << "TextToColor not implemented yet." << std::endl;
+    unsigned char values[3] = {0};
+    for (int i = 0; *text != '\0'; ++text) {
+        if (*text == ':') {
+            ++i;
+            continue;
+        }
+
+        if (*text < '0' || *text > '9')
+            break;
+
+        values[i] *= 10;
+        values[i] += *text - '0';
+    }
 
     ptg_color color;
-    color.r = 0;
-    color.g = 0;
-    color.b = 0;
+    color.r = values[0];
+    color.g = values[1];
+    color.b = values[2];
 
     return color;
 }
