874c461f5eb5aa5eb381895dfa7cdad66f61e38c
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-13 16:51:47 +0100

    Convert from sRGB to linear

--- src/quantization/color_conversion.cpp
+++ src/quantization/color_conversion.cpp
@@ -1,9 +1,20 @@
 #include "color_conversion.hpp"
 
+#include <cmath>
+
 cie_xyz rgb_to_xyz(const ptg_color& color) {
     cie_xyz result;
 
-    // TODO: Implement.
+    double r = color.r;
+    double g = color.g;
+    double b = color.b;
+
+    // Convert from sRGB to linear.
+    r = srgb_to_linear(r);
+    g = srgb_to_linear(g);
+    b = srgb_to_linear(b);
+
+    // TODO: Convert to XYZ.
 
     return result;
 }
@@ -14,4 +25,11 @@ cie_lab xyz_to_lab(const cie_xyz& color) {
     // TODO: Implement.
 
     return result;
+}
+
+double srgb_to_linear(double c) {
+    double a = 0.055;
+    double cutoff = 0.04045;
+
+    return c > cutoff ? pow(c + a / (1.0 + a), 2.4) : (c / 12.92);
 }
