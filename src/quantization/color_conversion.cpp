846e47bce80d0bc9d0a75194548f8d6625222442
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-14 10:38:37 +0100

    Implement RGB to XYZ conversion

--- src/quantization/color_conversion.cpp
+++ src/quantization/color_conversion.cpp
@@ -10,7 +10,17 @@ cie_xyz rgb_to_xyz(const ptg_color& color) {
     double g = srgb_to_linear(color.g);
     double b = srgb_to_linear(color.b);
 
-    // TODO: Convert to XYZ.
+    // Convert to XYZ, assuming D65 illuminant with a 2 degree standard observer.
+    // https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_(CIE_XYZ_to_sRGB)
+    // Retrieved 2018-02-14
+    result.x = 0.4124 * r + 0.3576 * g + 0.1805 * b;
+    result.y = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    result.z = 0.0193 * r + 0.1192 * g + 0.9505 * b;
+
+    // Scale values from normalized Y=1 to Y=100.
+    result.x *= 100;
+    result.y *= 100;
+    result.z *= 100;
 
     return result;
 }
@@ -25,7 +35,7 @@ cie_lab xyz_to_lab(const cie_xyz& color) {
 
 double srgb_to_linear(unsigned char component) {
     // https://en.wikipedia.org/wiki/SRGB#The_reverse_transformation
-
+    // Retrieved 2018-02-14
     double a = 0.055;
     double cutoff = 0.04045;
 
