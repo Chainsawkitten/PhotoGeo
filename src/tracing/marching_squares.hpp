712fe70c0af206fb5816e44ca01eadc1392b6a59
ivar <ivarbjorling@hotmail.com>
2018-02-14 09:25:17 +0100

    Minor fixes
    Fixed comments on pull request

--- src/tracing/marching_squares.hpp
+++ src/tracing/marching_squares.hpp
@@ -1,15 +1,13 @@
-#ifndef MARCHING_SQUARE_HPP
-#define MARCHING_SQUARE_HPP
+#ifndef MARCHING_SQUARES_HPP
+#define MARCHING_SQUARES_HPP
 
 #include <photogeo.h>
 
 /**
  * Trace image using marching squares.
- * @param layers Color layers.
- * @param layer_count The number of color layers.
  * @param image_parameters Source image parameters.
- * @param tracing_parameters Tracing parameters.
- * @param tracing_results Variable to store tracing results.
+ * @param quantization_results Quantization parameters.
+ * @param out_tracing_results Variable to store tracing results.
  */
 void trace_marching_squares(const ptg_image_parameters* image_parameters, const ptg_quantization_results* quantization_results, ptg_tracing_results* out_tracing_results);
 
