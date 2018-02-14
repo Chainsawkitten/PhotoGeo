bfabc520ef9d5c597916c825f1fb9eee0f353c33
ivar <ivarbjorling@hotmail.com>
2018-02-14 15:51:37 +0100

    Create lines

--- src/tracing/marching_squares.cpp
+++ src/tracing/marching_squares.cpp
@@ -2,11 +2,37 @@
 
 #include <vector>
 
-// Contains two vertices creating a line.
+// Forward declarations.
+struct line;
+struct vertex;
+
+// Straight line between two vertices.
 struct line {
-    // The vertices.
-    ptg_vec2* vertices[2];
+    // Vertex indices.
+    std::size_t vertex_indices[2];
 };
+
+// Vertex contaning position and line index.
+struct vertex {
+    // The postion of the vertex.
+    ptg_vec2 position;
+
+    // Line index which this vertex belongs.
+    std::size_t line_index;
+};
+
+/*
+ * Create vertices and add line to vectors.
+ * @param v1 First vertex.
+ * @param v2 Second vertex.
+ * @param out_lines Vector to store resulting lines.
+ * @param out_vertices Vector to store resulting vertices.
+ */
+static void add_line(const ptg_vec2& v1, const ptg_vec2& v2, std::vector<line>& out_lines, std::vector<vertex>& out_vertices) {
+    out_lines.push_back({ out_vertices.size(), out_vertices.size() + 1 });
+    out_vertices.push_back({ v1, out_lines.size() });
+    out_vertices.push_back({ v2, out_lines.size() });
+}
 
 /*
  * Marching squares algorithm.
@@ -19,7 +45,7 @@ struct line {
  * @param out_lines Vector to store resulting lines.
  * @param out_vertices Vector to store resulting vertices.
  */
-static void marching_kernel(bool topLeft, bool topRight, bool bottomRight, bool bottomLeft, unsigned int x, unsigned int y, std::vector<line>& out_lines, std::vector<ptg_vec2>& out_vertices) {
+static void marching_squares(bool topLeft, bool topRight, bool bottomRight, bool bottomLeft, unsigned int x, unsigned int y, std::vector<line>& out_lines, std::vector<vertex>& out_vertices) {
 
     // Calculate current configuration of marching squares.
     const unsigned int square_configuration = topLeft * 8 + topRight * 4 + bottomRight * 2 + bottomLeft * 1;
@@ -36,6 +62,9 @@ static void marching_kernel(bool topLeft, bool topRight, bool bottomRight, bool 
     const ptg_vec2 C = { x, y + 1 };
     const ptg_vec2 D = { x - 1, y };
 
+    std::size_t line_index = out_lines.size();
+    std::size_t vertex_index = out_vertices.size();
+
     switch (square_configuration) {
         // 0 points:    
         case 0:
@@ -44,95 +73,78 @@ static void marching_kernel(bool topLeft, bool topRight, bool bottomRight, bool 
         // 1 points:
         // C -> D
         case 1:
-            out_vertices.push_back(C);
-            out_vertices.push_back(D);
+            add_line(C, D, out_lines, out_vertices);
             break;
 
         // B -> C
         case 2:
-            out_vertices.push_back(B);
-            out_vertices.push_back(C);
+            add_line(B, C, out_lines, out_vertices);
             break;
 
         // A -> B
         case 4:
-            out_vertices.push_back(A);
-            out_vertices.push_back(B);
+            add_line(A, B, out_lines, out_vertices);
             break;
 
         // D -> A
         case 8:
-            out_vertices.push_back(D);
-            out_vertices.push_back(A);
+            add_line(D, A, out_lines, out_vertices);
             break;
 
         // 2 points:
         // B -> D
         case 3:
-            out_vertices.push_back(B);
-            out_vertices.push_back(D);
+            add_line(B, D, out_lines, out_vertices);
             break;
         
         // A -> C
         case 6:
-            out_vertices.push_back(A);
-            out_vertices.push_back(C);
+            add_line(A, C, out_lines, out_vertices);
             break;
 
         // C -> A
         case 9:
-            out_vertices.push_back(C);
-            out_vertices.push_back(A);
+            add_line(C, A, out_lines, out_vertices);
             break;
 
         // D -> B
         case 12:
-            out_vertices.push_back(D);
-            out_vertices.push_back(B);
+            add_line(D, B, out_lines, out_vertices);
             break;
 
         // 2 points (diagonal):
         // B -> C, D -> A
         case 5:
-            out_vertices.push_back(B);
-            out_vertices.push_back(C);
-
-            out_vertices.push_back(D);
-            out_vertices.push_back(A);
+            add_line(B, C, out_lines, out_vertices);
+            add_line(D, A, out_lines, out_vertices);
             break;
 
         // A -> B, C -> D
         case 10:
-            out_vertices.push_back(A);
-            out_vertices.push_back(B);
+            add_line(A, B, out_lines, out_vertices);
+            add_line(C, D, out_lines, out_vertices);
 
-            out_vertices.push_back(C);
-            out_vertices.push_back(D);
             break;
 
         // 3 points:
         // D -> A
         case 7:
-            out_vertices.push_back(D);
-            out_vertices.push_back(A);
+            add_line(D, A, out_lines, out_vertices);
             break;
 
         // A -> B
         case 11:
-            out_vertices.push_back(A);
-            out_vertices.push_back(B);
+            add_line(A, B, out_lines, out_vertices);
             break;
 
         // B -> C
         case 13:
-            out_vertices.push_back(B);
-            out_vertices.push_back(C);
+            add_line(B, C, out_lines, out_vertices);
             break;
 
         // C -> D
         case 14:
-            out_vertices.push_back(C);
-            out_vertices.push_back(D);
+            add_line(C, D, out_lines, out_vertices);
             break;
 
         // 4 points:
@@ -144,12 +156,12 @@ static void marching_kernel(bool topLeft, bool topRight, bool bottomRight, bool 
 void ptgi_trace_marching_squares(bool* layer, unsigned int layer_width, unsigned int layer_height, ptg_outline*& out_outlines, unsigned int& out_outline_count) {
 
     std::vector<line> lines;
-    std::vector<ptg_vec2> vertices;
+    std::vector<vertex> vertices;
 
     // Execute marching squares on layer.
     for (unsigned int it = 0; it < layer_width * (layer_height - 1); ++it)
         if ((it + 1) % layer_width != 0)
-            marching_kernel(layer[it], layer[it + 1], layer[it + layer_width], layer[it + layer_width + 1], it % layer_width + 1, it / layer_width + 1, lines, vertices);
+            marching_squares(layer[it], layer[it + 1], layer[it + layer_width], layer[it + layer_width + 1], it % layer_width + 1, it / layer_width + 1, lines, vertices);
 
     // Create outlines.
     out_outline_count = static_cast<unsigned int>(vertices.size() / 2);
@@ -157,7 +169,7 @@ void ptgi_trace_marching_squares(bool* layer, unsigned int layer_width, unsigned
     for (unsigned int outline_index = 0; outline_index < out_outline_count; ++outline_index) {
         out_outlines[outline_index].vertex_count = 2;
         out_outlines[outline_index].vertices = new ptg_vec2[2];
-        out_outlines[outline_index].vertices[0] = vertices[outline_index * 2];
-        out_outlines[outline_index].vertices[1] = vertices[outline_index * 2 + 1];
+        out_outlines[outline_index].vertices[0] = vertices[outline_index * 2].position;
+        out_outlines[outline_index].vertices[1] = vertices[outline_index * 2 + 1].position;
     }
 }
