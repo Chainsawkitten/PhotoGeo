3e31235daeb9fcb7d673c5580b5634278264e388
ivar <ivarbjorling@hotmail.com>
2018-02-16 15:11:54 +0100

    Trace contours to generate outlines

--- src/tracing/marching_squares.cpp
+++ src/tracing/marching_squares.cpp
@@ -1,5 +1,6 @@
 #include "marching_squares.hpp"
 
+#include <assert.h>
 #include <vector>
 
 // Forward declarations.
@@ -19,6 +20,9 @@ struct vertex {
 
     // Line index which this vertex belongs.
     std::size_t line_index;
+
+    // Whether vertex is assigned a contour.
+    bool assigned_contour;
 };
 
 /*
@@ -30,8 +34,8 @@ struct vertex {
  */
 static void add_line(const ptg_vec2& v1, const ptg_vec2& v2, std::vector<line>& out_lines, std::vector<vertex>& out_vertices) {
     out_lines.push_back({ out_vertices.size(), out_vertices.size() + 1 });
-    out_vertices.push_back({ v1, out_lines.size() });
-    out_vertices.push_back({ v2, out_lines.size() });
+    out_vertices.push_back({ v1, out_lines.size() - 1, false });
+    out_vertices.push_back({ v2, out_lines.size() - 1, false });
 }
 
 /*
@@ -153,13 +157,63 @@ static void marching_squares(bool topLeft, bool topRight, bool bottomRight, bool
     }
 }
 
-void ptgi_trace_marching_squares(bool* layer, unsigned int layer_width, unsigned int layer_height, ptg_outline*& out_outlines, unsigned int& out_outline_count) {
+/*
+ * Find other vertex with same position.
+ * @param this_index Vertex index of vertex to find other vertex with same position.
+ * @param vertices The vertices.
+ * @param out_found_index Variable to store vertex index of the found vertex.
+ */
+static bool find_equal_vertex(const std::size_t this_index, const std::vector<vertex>& vertices, std::size_t& out_found_index) {
+    const vertex& this_vertex = vertices[this_index];
+    for (std::size_t vertex_index = 0; vertex_index < vertices.size(); ++vertex_index) {
+        const vertex& vertex = vertices[vertex_index];
+        if (this_index != vertex_index && vertex.position.x == this_vertex.position.x && this_vertex.position.y == vertex.position.y) {
+            out_found_index = vertex_index;
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+ * Tracing along contour to find all vertices of an outline.
+ * @param vertex_index Vertex index of vertex to find other vertex with same position.
+ * @param vertices The vertices generated from marching squares.
+ * @param lines The lines generated from marching squares.
+ * @param out_contour Vector to store vertices.
+ */
+static void trace_contour(const std::size_t vertex_index, std::vector<vertex>& vertices, const std::vector<line>& lines, std::vector<std::size_t>& out_contour) {
+    // Add vertex to contour.
+    out_contour.push_back(vertex_index);
+
+    // Find connected vertex on line.
+    const line& line = lines[vertices[vertex_index].line_index];
+    std::size_t connected_vertex_index = line.vertex_indices[0] == vertex_index ? line.vertex_indices[1] : line.vertex_indices[0];
+    
+    // Set vertices as assigned to contour.
+    vertices[vertex_index].assigned_contour = true;
+    vertices[connected_vertex_index].assigned_contour = true;
+
+    // Find other vertex starting at connected vertex.
+    std::size_t found_vertex_index;
+    find_equal_vertex(connected_vertex_index, vertices, found_vertex_index);
+
+    // Check whether found vertex is assigned.
+    if (!vertices[found_vertex_index].assigned_contour) {
+        // Continue to trace contour.
+        trace_contour(found_vertex_index, vertices, lines, out_contour);
+    } else {
+        // Finnish contour by connecting tail and root.
+        out_contour.push_back(out_contour.front());
+    }
+}
+
+void trace_marching_squares(bool* layer, unsigned int layer_width, unsigned int layer_height, ptg_outline*& out_outlines, unsigned int& out_outline_count) {
 
     std::vector<line> lines;
     std::vector<vertex> vertices;
 
     // Execute marching squares on layer.
-    for (unsigned int it = 0; it < layer_width * layer_height; ++it) {
     for (unsigned int it = 0; it < (layer_width * layer_height); ++it) {
         bool topEdge = it < layer_width;
         bool bottomEdge = it >= (layer_width * (layer_height - 1));
@@ -174,14 +228,35 @@ void ptgi_trace_marching_squares(bool* layer, unsigned int layer_width, unsigned
         marching_squares(topLeft, topRight, bottomRight, bottomLeft, it % layer_width, it / layer_width, lines, vertices);
     }
 
+    // Create contours.
+    std::vector<std::vector<std::size_t>> contours;
+    for (std::size_t vertex_index = 0; vertex_index < vertices.size(); ++vertex_index) {
+        if (!vertices[vertex_index].assigned_contour) {
+            // Create new contour.
+            contours.push_back(std::vector<std::size_t>());
+            // Trace contour (assign connected vertex indices to contour).
+            trace_contour(vertex_index, vertices, lines, contours.back());
+        }
+    }
 
     // Create outlines.
-    out_outline_count = static_cast<unsigned int>(vertices.size() / 2);
+    out_outline_count = (unsigned int)contours.size();
     out_outlines = new ptg_outline[out_outline_count];
-    for (unsigned int outline_index = 0; outline_index < out_outline_count; ++outline_index) {
-        out_outlines[outline_index].vertex_count = 2;
-        out_outlines[outline_index].vertices = new ptg_vec2[2];
-        out_outlines[outline_index].vertices[0] = vertices[outline_index * 2].position;
-        out_outlines[outline_index].vertices[1] = vertices[outline_index * 2 + 1].position;
+    for (std::size_t countour_index = 0; countour_index < contours.size(); ++countour_index) {
+        std::vector<std::size_t>& contour = contours[countour_index];
+        ptg_outline& out_outline = out_outlines[(unsigned int)countour_index];
+        out_outline.vertex_count = (unsigned int)contour.size();
+        out_outline.vertices = new ptg_vec2[out_outline.vertex_count];
+        for (std::size_t vertex_index = 0; vertex_index < contour.size(); ++vertex_index) {
+            out_outline.vertices[(unsigned int)vertex_index] = vertices[contour[vertex_index]].position;
+        }
     }
+    //out_outline_count = static_cast<unsigned int>(vertices.size() / 2);
+    //out_outlines = new ptg_outline[out_outline_count];
+    //for (unsigned int outline_index = 0; outline_index < out_outline_count; ++outline_index) {
+    //    out_outlines[outline_index].vertex_count = 2;
+    //    out_outlines[outline_index].vertices = new ptg_vec2[2];
+    //    out_outlines[outline_index].vertices[0] = vertices[outline_index * 2].position;
+    //    out_outlines[outline_index].vertices[1] = vertices[outline_index * 2 + 1].position;
+    //}
 }
