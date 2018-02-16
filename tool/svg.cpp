5facced8d86ea7ab54670acf19d4b79f8b9303ed
ivar <ivarbjorling@hotmail.com>
2018-02-16 16:44:10 +0100

    Write path to svg

--- tool/svg.cpp
+++ tool/svg.cpp
@@ -13,7 +13,8 @@ void write_svg(const char* filename, unsigned int layers, ptg_color* colors, ptg
 
     // Write header.
     file << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n"
-         << "<svg xmlns=\"http://www.w3.org/2000/svg\">\n";
+         << "<svg xmlns=\"http://www.w3.org/2000/svg\"\n"
+         << "xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\">\n";
 
     // Layers.
     for (unsigned int layer = 0; layer < layers; ++layer) {
@@ -23,26 +24,25 @@ void write_svg(const char* filename, unsigned int layers, ptg_color* colors, ptg
         for (unsigned int outline_index = 0; outline_index < outline_counts[layer]; ++outline_index) {
             const ptg_outline& outline = outlines[layer][outline_index];
 
-            // Lines.
-            for (unsigned int vertex_index = 0; vertex_index < outline.vertex_count - 1; ++vertex_index) {
-                file << "    <line x1=\"" << outline.vertices[vertex_index].x
-                    << "\" y1=\"" << outline.vertices[vertex_index].y
-                    << "\" x2=\"" << outline.vertices[vertex_index + 1].x
-                    << "\" y2=\"" << outline.vertices[vertex_index + 1].y
-                    << "\" stroke-width=\"2\" stroke=\"rgb(" << (int)colors[layer].r << ", " << (int)colors[layer].g << ", " << (int)colors[layer].b << ")\" />\n";
-            }
-
+            // Path.
+            file << "    <path\n"
+                 << "       style=\"fill:none;stroke:rgb(" << (int)colors[layer].r << ", " << (int)colors[layer].g << ", " << (int)colors[layer].b << ");stroke-width:2px\"\n"
+                 << "       inkscape:connector-curvature=\"0\"\n"
+                 << "       d=\"M";
+            for (unsigned int vertex_index = 0; vertex_index < outline.vertex_count; ++vertex_index)
+                file << " " << outline.vertices[vertex_index].x << "," << outline.vertices[vertex_index].y;
+            file << " Z\" />\n";
+        
             // Markers.
             if (markers) {
                 for (unsigned int vertex_index = 0; vertex_index < outline.vertex_count; ++vertex_index) {
                     file << "    <rect x=\"" << (int)outline.vertices[vertex_index].x - 2
-                        << "\" y=\"" << (int)outline.vertices[vertex_index].y - 2
-                        << "\" width=\"4\" height=\"4\""
-                        << " stroke-width=\"0\" fill=\"rgb(" << (int)colors[layer].r << ", " << (int)colors[layer].g << ", " << (int)colors[layer].b << ")\" />\n";
+                         << "\" y=\"" << (int)outline.vertices[vertex_index].y - 2
+                         << "\" width=\"4\" height=\"4\""
+                         << " stroke-width=\"0\" fill=\"rgb(" << (int)colors[layer].r << ", " << (int)colors[layer].g << ", " << (int)colors[layer].b << ")\" />\n";
                 }
             }
         }
-
         file << "  </g>\n";
     }
 
