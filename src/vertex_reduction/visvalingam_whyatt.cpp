faf3ac1499e4113ce2b98f0102d653b63305f599
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-19 15:30:43 +0100

    Visvalingam-Whyatt stub

---
+++ src/vertex_reduction/visvalingam_whyatt.cpp
@@ -0,0 +1,7 @@
+#include "visvalingam_whyatt.hpp"
+
+#include <iostream>
+
+void ptgi_visvalingam_whyatt(ptg_tracing_results* tracing_results) {
+    std::cerr << "ptg_visvalingam_whyatt has not yet been implemented." << std::endl;
+}
