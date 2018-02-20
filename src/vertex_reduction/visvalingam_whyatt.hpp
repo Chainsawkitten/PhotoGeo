48656747c09bfe6b68e732590ac04fe0035410c9
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-20 13:32:41 +0100

    Cleanup

--- src/vertex_reduction/visvalingam_whyatt.hpp
+++ src/vertex_reduction/visvalingam_whyatt.hpp
@@ -6,6 +6,9 @@
 /**
  * Reduce vertex geometry using Visvalingam-Whyatt.
  * @param tracing_results Results from the tracing step.
+ * @todo Optimize using min-heap.
+ * @todo Handle complete removal of outline.
+ * @todo Look into resizing result array (realloc).
  */
 void ptgi_visvalingam_whyatt(ptg_tracing_results* tracing_results);
 
