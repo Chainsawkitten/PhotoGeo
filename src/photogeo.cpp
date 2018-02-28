2144ff3f80812c98e3d75e2b158c669ac540101e
Albin Bernhardsson <catscratchesp@gmail.com>
2018-02-28 15:25:22 +0100

    Output results in library
    Output results from ptg_generate_collision_geometry.

--- src/photogeo.cpp
+++ src/photogeo.cpp
@@ -6,8 +6,6 @@
 #include "vertex_reduction/visvalingam_whyatt.hpp"
 
 void ptg_generate_collision_geometry(const ptg_generation_parameters* parameters, ptg_outline*** out_outlines, unsigned int** out_outline_counts) {
-    std::cerr << "ptg_generate_collision_geometry has not yet been implemented." << std::endl;
-
     // Quantization.
     ptg_quantization_results quantization_results;
     ptg_quantize(parameters->image_parameters, parameters->quantization_parameters, &quantization_results);
@@ -16,15 +14,14 @@ void ptg_generate_collision_geometry(const ptg_generation_parameters* parameters
     ptg_tracing_results tracing_results;
     ptg_trace(parameters->image_parameters, &quantization_results, parameters->tracing_parameters, &tracing_results);
 
+    // Quantization results are no longer needed.
+    ptg_free_quantization_results(&quantization_results);
+
     // Vertex reduction.
     ptg_reduce(&tracing_results, parameters->vertex_reduction_parameters);
 
-    // Free results.
-    ptg_free_tracing_results(&tracing_results);
-    ptg_free_quantization_results(&quantization_results);
-
-    *out_outlines = nullptr;
-    *out_outline_counts = nullptr;
+    *out_outlines = tracing_results.outlines;
+    *out_outline_counts = tracing_results.outline_counts;
 }
 
 void ptg_free_results(ptg_outline** outlines, unsigned int* outline_count) {
