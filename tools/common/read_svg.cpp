74780142422811ac4ebd7018afd2ad799e37c591
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-08 13:43:40 +0100

    read_svg stub

--- tools/common/read_svg.cpp
+++ tools/common/read_svg.cpp
@@ -1,1 +1,14 @@
 #include "read_svg.hpp"
+
+#include <iostream>
+
+ptg_tracing_results read_svg(const char* filename) {
+    std::cerr << "read_svg has not yet been implemented." << std::endl;
+
+    ptg_tracing_results results;
+    results.layer_count = 0;
+    results.outline_counts = nullptr;
+    results.outlines = nullptr;
+
+    return results;
+}
