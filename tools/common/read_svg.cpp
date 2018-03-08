a0d96b963da78723ea70a5a2b1c037bfff0f0440
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-08 14:17:29 +0100

    Parse SVG file

--- tools/common/read_svg.cpp
+++ tools/common/read_svg.cpp
@@ -1,9 +1,17 @@
 #include "read_svg.hpp"
 
 #include <iostream>
+#include <tinyxml2.h>
 
 ptg_tracing_results read_svg(const char* filename) {
     std::cerr << "read_svg has not yet been implemented." << std::endl;
+
+    // Parse SVG file.
+    tinyxml2::XMLDocument doc;
+    doc.LoadFile(filename);
+
+    // TODO Parse layers.
+    // TODO Parse contours.
 
     ptg_tracing_results results;
     results.layer_count = 0;
