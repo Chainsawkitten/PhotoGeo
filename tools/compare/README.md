93347a62aa68eadf75c4111052d8360f014aee13
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-08 11:00:53 +0100

    Compare tool stub

---
+++ tools/compare/README.md
@@ -0,0 +1,3 @@
+# Compare
+
+Tool to compare two vector contour images and output the degree of similarity.
---
+++ tools/compare/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+    std::cout << "Compare tool not yet implemented." << std::endl;
+
+    return 0;
+}
