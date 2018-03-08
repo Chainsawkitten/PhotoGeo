9f18f3761c0fba785b958a62970a7b6f51576f97
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-08 13:43:40 +0100

    read_svg stub

--- tools/common/read_svg.hpp
+++ tools/common/read_svg.hpp
@@ -1,6 +1,13 @@
 #ifndef READ_SVG_HPP
 #define READ_SVG_HPP
 
+#include <photogeo.h>
 
+/**
+ * Read and parse SVG file.
+ * @param filename The filename of the file to read.
+ * @return The contours in the SVG file.
+ */
+ptg_tracing_results read_svg(const char* filename);
 
 #endif
