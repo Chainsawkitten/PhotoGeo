ccc96db21ed29b85bad37d9a6545800e2747caef
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-12 11:43:39 +0100

    Point in polygon

--- tools/rasterize/rasterize.cpp
+++ tools/rasterize/rasterize.cpp
@@ -1,20 +1,39 @@
 #include "rasterize.hpp"
 
 #include <iostream>
+#include <cstring>
 
 void rasterize(const ptg_tracing_results* tracing_results, unsigned int width, unsigned int height, ptg_color* image) {
     std::cerr << "rasterize has not yet implemented." << std::endl;
 
-    // Loop through all rows.
-    for (unsigned int y = 0; y < height; ++y) {
-        // TODO: Perform raytracing.
+    // White background color.
+    for (unsigned int i = 0; i < width * height; ++i)
+        image[i] = {255, 255, 255};
 
-        // Write pixel values.
-        for (unsigned int x = 0; x < width; ++x) {
-            // White background color.
-            image[y * width + x] = {255, 255, 255};
+    bool* collision = new bool[width];
 
-            // TODO: Loop through all layers and check point in polygon problem.
+    // Loop through all layers.
+    for (unsigned int layer = 0; layer < tracing_results->layer_count; ++layer) {
+        // Loop through all rows.
+        for (unsigned int y = 0; y < height; ++y) {
+            memset(collision, 0, width);
+
+            // TODO: Perform raytracing.
+
+            // Write pixel values.
+            bool inside = false;
+            for (unsigned int x = 0; x < width; ++x) {
+                // Check point in polygon problem.
+                if (collision[x])
+                    inside = !inside;
+
+                if (inside)
+                    // TODO: Use the color of the layer rather than hard-coded black.
+                    image[y * width + x] = {0, 0, 0};
+            }
         }
     }
+
+    // Clean up.
+    delete[] collision;
 }
