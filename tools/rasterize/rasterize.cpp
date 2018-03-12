bbad75ca4fe2f50b63fa04da8c264dd5c9a1899e
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-12 11:10:18 +0100

    Write background color

--- tools/rasterize/rasterize.cpp
+++ tools/rasterize/rasterize.cpp
@@ -4,4 +4,17 @@
 
 void rasterize(const ptg_tracing_results* tracing_results, unsigned int width, unsigned int height, ptg_color* image) {
     std::cerr << "rasterize has not yet implemented." << std::endl;
+
+    // Loop through all rows.
+    for (unsigned int y = 0; y < height; ++y) {
+        // TODO: Perform raytracing.
+
+        // Write pixel values.
+        for (unsigned int x = 0; x < width; ++x) {
+            // White background color.
+            image[y * width + x] = {255, 255, 255};
+
+            // TODO: Loop through all layers and check point in polygon problem.
+        }
+    }
 }
