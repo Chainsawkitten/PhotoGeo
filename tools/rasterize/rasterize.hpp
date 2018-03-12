24ba485cc32e45b2b654f2da81275af15b6e6908
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-12 10:54:07 +0100

    rasterize stub

---
+++ tools/rasterize/rasterize.hpp
@@ -0,0 +1,15 @@
+#ifndef RASTERIZE_HPP
+#define RASTERIZE_HPP
+
+#include <photogeo.h>
+
+/**
+ * Rasterize vector contours.
+ * @param tracing_results Contours to rasterize.
+ * @param width The width of the image in pixels.
+ * @param height The height of the image in pixels.
+ * @param image The image to write results to.
+ */
+void rasterize(const ptg_tracing_results* tracing_results, unsigned int width, unsigned int height, ptg_color* image);
+
+#endif
