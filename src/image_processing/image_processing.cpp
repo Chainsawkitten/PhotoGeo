13bc457fa666960e0274a64bbeaaa6a77ed9e4df
ivar <ivarbjorling@hotmail.com>
2018-03-15 15:37:08 +0100

    Slash error

--- src/image_processing/image_processing.cpp
+++ src/image_processing/image_processing.cpp
@@ -1,7 +1,7 @@
 #include "image_processing.hpp"
 
 #include <cstring>
-#include <opencv2\imgproc.hpp>
+#include <opencv2/imgproc.hpp>
 
 void ptg_image_process(const ptg_image_parameters* image_parameters, const ptg_image_processing_parameters* image_processing_parameters) {
     cv::Mat src = cv::Mat(image_parameters->height, image_parameters->width, CV_8UC3, image_parameters->image);
