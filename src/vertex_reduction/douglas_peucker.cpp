ac77b31b21fa272178efdd075f97ae51ac77617c
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-28 16:08:19 +0200

    Find the two points farthest from each other

--- src/vertex_reduction/douglas_peucker.cpp
+++ src/vertex_reduction/douglas_peucker.cpp
@@ -7,7 +7,28 @@
  * @param outline The outline to reduce.
  */
 static void reduce_outline(ptg_outline& outline) {
-    // TODO: Implement.
+    // Find the two points farthest from each other.
+    unsigned int max_first_point = 0;
+    unsigned int max_last_point = 0;
+    double max_distance = 0.0;
+    for (unsigned int first_point = 0; first_point < outline.vertex_count - 1; ++first_point) {
+        for (unsigned int last_point = first_point + 1; last_point < outline.vertex_count; ++last_point) {
+            const ptg_vec2 p1 = outline.vertices[first_point];
+            const ptg_vec2 p2 = outline.vertices[last_point];
+            const double delta_x = p2.x - p1.x;
+            const double delta_y = p2.y - p1.y;
+            const double distance = delta_x * delta_x + delta_y * delta_y;
+            if (distance > max_distance) {
+                max_distance = distance;
+                max_first_point = first_point;
+                max_last_point = last_point;
+            }
+        }
+    }
+
+    // TODO: Allocate buffer for whether points should be kept.
+    // TODO: Apply Douglas-Peucker recursively on both lines.
+    // TODO: Generate final outline.
 }
 
 void ptgi_douglas_peucker(ptg_tracing_results* tracing_results) {
