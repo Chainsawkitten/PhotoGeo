023f0c9bc9562adec3257b895d0eab6c40b309dd
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-28 10:35:09 +0200

    Run multiple image processing methods
    Take in image processing methods to use as an array rather than a single enum. This allows us to run many (or no) image processing methods in a specified order.

--- src/image_processing/image_processing.cpp
+++ src/image_processing/image_processing.cpp
@@ -4,12 +4,14 @@
 #include <opencv2/imgproc.hpp>
 
 void ptgi_image_process(const ptg_image_parameters* image_parameters, const ptg_image_processing_parameters* image_processing_parameters) {
-    switch (image_processing_parameters->image_processing_method) {
-        case PTG_GAUSSIAN_BLUR:
-            cv::Mat src = cv::Mat(image_parameters->height, image_parameters->width, CV_8UC3, image_parameters->image);
-            cv::Mat dst = cv::Mat(image_parameters->height, image_parameters->width, CV_8UC3);
-            cv::filter2D(src, dst, -1, cv::getGaussianKernel(3, 0.5), cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
-            memcpy(image_parameters->image, dst.data, image_parameters->width * image_parameters->height * sizeof(ptg_color));
-            break;
+    for (unsigned int i = 0; i < image_processing_parameters->method_count; ++i) {
+        switch (image_processing_parameters->methods[i]) {
+            case PTG_GAUSSIAN_BLUR:
+                cv::Mat src = cv::Mat(image_parameters->height, image_parameters->width, CV_8UC3, image_parameters->image);
+                cv::Mat dst = cv::Mat(image_parameters->height, image_parameters->width, CV_8UC3);
+                cv::filter2D(src, dst, -1, cv::getGaussianKernel(3, 0.5), cv::Point(-1, -1), 0, cv::BORDER_DEFAULT);
+                memcpy(image_parameters->image, dst.data, image_parameters->width * image_parameters->height * sizeof(ptg_color));
+                break;
+        }
     }
 }
