8635586721ba6b1d0cfa1a1d7edbf706031fce68
ivar <ivarbjorling@hotmail.com>
2018-03-30 13:54:46 +0200

    Updated header
    Removed todo

--- src/image_processing/image_processing.hpp
+++ src/image_processing/image_processing.hpp
@@ -7,7 +7,6 @@
  * Process image.
  * @param image_parameters Image input parameters.
  * @param Parameters regarding which methods to use during image processing.
- * @todo Implement this. Currently does nothing.
  */
 void ptgi_image_process(const ptg_image_parameters* image_parameters, const ptg_image_processing_parameters* image_processing_parameters);
 
