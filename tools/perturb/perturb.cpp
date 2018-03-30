73bd606cee3c06b2dcd22d8f0a9055a21c7f77a9
Albin Bernhardsson <catscratchesp@gmail.com>
2018-03-30 16:22:48 +0200

    perturb stub

---
+++ tools/perturb/perturb.cpp
@@ -0,0 +1,17 @@
+#include "perturb.hpp"
+
+#include <iostream>
+
+void perturb(unsigned char* data, unsigned int width, unsigned int height) {
+    std::cout << "perturb has not yet been implemented." << std::endl;
+
+    // TODO: Make alpha channel (background = transparent, foreground = opaque).
+    // TODO: Replace background color with black.
+    // TODO: Gaussian blur.
+    // TODO: Downscale.
+    // TODO: Divide color channels with alpha channel.
+    // TODO: Load marker texture and tile.
+    // TODO: Multiply alpha channel and marker texture.
+    // TODO: Load paper texture and scale.
+    // TODO: Blend image with paper texture according to alpha channel.
+}
