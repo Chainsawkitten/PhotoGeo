14623b3f3e598d4813a73a6f9e3b13fb553779e9
Albin Bernhardsson <catscratchesp@gmail.com>
2018-04-13 11:55:59 +0200

    Store results

--- test/test.bat
+++ test/test.bat
@@ -1,6 +1,12 @@
 @Echo Off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+:: Run test.
 CALL photogeo_profile.bat %*
 CALL rasterize.bat
 CALL compare.bat
+
+:: Store results.
+MKDIR "results/%*"
+MOVE compare "results/%*/compare"
+MOVE photogeo_profile "results/%*/photogeo_profile"
